d5ddaac67ca4fa938dcc881924fc14dd59978fa9
Rakib Mahmud <fiverrrakib2017@gmail.com>
2024-10-01 11:15:20 +0600

    student seeder completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            StudentSeeder::class
+        ]);
         \App\Models\Admin::create([
             'name' => 'Rakib Mahmud',
             'email' => 'rakibas375@gmail.com',
