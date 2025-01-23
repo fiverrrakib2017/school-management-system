5520bfa19474e173aede62d1262d2d3ebd9d4a3f
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-01-23 16:47:53 +0600

    authentication problem solve

--- resources/views/Backend/Pages/Login/Login.blade.php
+++ resources/views/Backend/Pages/Login/Login.blade.php
@@ -1,3 +1,4 @@
+
 @if (auth()->guard('admin')->check())
 
 <script>
@@ -5,114 +6,102 @@
 </script>
 
 @endif
-
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title>Log In | Adminto - Responsive Admin Dashboard Template</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
-        <meta content="Coderthemes" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
 
-        <!-- App css -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Log In | School Management System </title>
 
-        <link href="{{asset('Backend/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" id="app-style" />
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link href="{{asset('Backend/plugins/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- icheck bootstrap -->
+    <link href="{{asset('Backend/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="{{asset('Backend/dist/css/adminlte.min.css?v=3.2.0')}}" rel="stylesheet" type="text/css" />
 
-        <!-- icons -->
-        <link href="{{asset('Backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="assets/css/toastr.min.css" rel="stylesheet" type="text/css">
-    </head>
+    <style>
+        .login-box .card,
+        .register-box .card {
 
-    <body class="loading authentication-bg authentication-bg-pattern">
+            border: 2px #838383 dotted !important;
+        }
+        .login-card-body, .register-card-body{
+            border-bottom: 20px !important;
+        }
+    </style>
+</head>
 
-        <div class="account-pages ">
-            <div class="container">
+<body class="hold-transition register-page">
+    <div class="register-box">
+        <!-- <div class="register-logo">
+   <img  width="90px" src="http://103.146.16.154/profileImages/avatar.png">
+  </div> -->
 
-                <div class="row justify-content-center">
-                    <div class="col-md-8 col-lg-6 col-xl-4 mt-1">
-                        <div class="text-center">   
-                            <a href="index.html">
-                                <img src="{{asset('Backend/assets/images/it-fast.png')}}" alt=""  >
-                            </a>
-                            <p class="text-muted mt-2 mb-4">School Management System Admin Dashboard</p>
-
-                        </div>
-                        <div class="card">
-                         
-                          @if ($errors->any())
-                          <div class="card-header">
-                              <div class="alert alert-danger">
-                                  <ul>
-                                      @foreach ($errors->all() as $error)
-                                          <li>{{ $error }}</li>
-                                      @endforeach
-                                  </ul>
-                              </div>
-                          </div>
-                          @endif
-                          @if(Session::has('error-message'))
-                          <div class="card-header">
-                                <p class="alert alert-danger">{{ Session::get('error-message') }}</p>
-                          </div>
-                          @endif
-                          
-                            <div class="card-body p-4">
-                                
-                                <div class="text-center mb-4">
-                                    <h4 class="text-uppercase mt-0">Sign In</h4>
-                                </div>
-
-                                <form action="{{ route('login.functionality') }}" method="post">
-                                @csrf
-                                    <div class="mb-3">
-                                        <label for="emailaddress" class="form-label">Username</label>
-                                        <input type="email" class="form-control" name="email" placeholder="Enter  Email" value="{{old('email')}}">
-                                    </div>
-
-                                    <div class="mb-3">
-                                        <label for="password" class="form-label">Password</label>
-                                        <input type="password" class="form-control" name="password" placeholder="Enter Password">
-                                    </div>
-
-                                    <div class="mb-3">
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="checkbox-signin" checked>
-                                            <label class="form-check-label" for="checkbox-signin">Remember me</label>
-                                        </div>
-                                    </div>
-
-                                    <div class="mb-3 d-grid text-center">
-                                        <button class="btn btn-primary" type="submit"> Log In </button>
-                                    </div>
-                                </form>
-
-                            </div> <!-- end card-body -->
-                        </div>
-                        <!-- end card -->
-
-
-                    </div> <!-- end col -->
+        <div class="card">
+            @if ($errors->any())
+            <div class="card-header">
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
                 </div>
-                <!-- end row -->
             </div>
-            <!-- end container -->
-        </div>
-        <!-- end page -->
+            @endif
+            @if(Session::has('error-message'))
+            <div class="card-header">
+                  <p class="alert alert-danger">{{ Session::get('error-message') }}</p>
+            </div>
+            @endif
+            <div class="card-body register-card-body">
+                <div class="register-logo">
+                    <img width="90px" src="http://103.146.16.154/profileImages/avatar.png">
+                </div>
+                <p class="login-box-msg">Welcome Back</p>
 
-        <script src="{{ asset('Backend/assets/libs/jquery/jquery.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/libs/simplebar/simplebar.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/libs/node-waves/waves.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/libs/waypoints/lib/jquery.waypoints.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/libs/jquery.counterup/jquery.counterup.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/libs/feather-icons/feather.min.js') }}"></script>
-        <script src="{{ asset('Backend/assets/js/toastr.min.js') }}"></script>
-        <!-- App js -->
-        <script src="{{ asset('Backend/assets/js/app.min.js') }}"></script>
 
-    </body>
+                <form action="{{ route('login.functionality') }}" method="post">
+                    @csrf
+
+                    <div class="input-group mb-3">
+                        <input  type="email" name="email" class="form-control" placeholder="Enter Your Email" value="{{old('email')}}">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-user"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control" placeholder="Enter Your Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <button  type="submit" class="btn btn-block btn-success">
+                        Login  <i class="fas fa-sign-in-alt"></i>
+                    </button>
+                </form>
+            </div>
+            <!-- /.form-box -->
+        </div><!-- /.card -->
+    </div>
+    <!-- /.register-box -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('Backend/plugins/jquery/jquery.min.js') }}"></script>
+    <!-- Bootstrap 4 -->
+    <script src="https://adminlte.io/themes/v3/"></script>
+    <script src="{{ asset('Backend/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('Backend/dist/js/adminlte.min.js?v=3.2.0') }}"></script>
+
+</body>
+
 </html>
