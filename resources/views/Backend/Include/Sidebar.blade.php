31a93b7b62c76fe09f4fa9b616beb98f9bc7a098
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-01-27 16:33:56 +0600

    sidebar images issu fixed

--- resources/views/Backend/Include/Sidebar.blade.php
+++ resources/views/Backend/Include/Sidebar.blade.php
@@ -1,7 +1,7 @@
 
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="{{route('admin.dashboard')}}" class="brand-link">
-        <img src="http://103.146.16.154/assets/images/it-fast.png" alt="AdminLTE Logo" class="brand-image elevation-3" style="">
+        <img src="{{asset('Backend/images/it-fast.png')}}" alt="AdminLTE Logo" class="brand-image elevation-3" style="">
 
       </a>
     <!-- Sidebar -->
