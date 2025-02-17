801f8ad45ee5920f0c3705ae2dc25eb5fba2dcea
Rakib Mahmud <fiverrrakib2017@gmail.com>
2025-02-17 16:18:05 +0600

    website include

--- routes/web.php
+++ routes/web.php
@@ -45,13 +45,12 @@ use App\Http\Controllers\Backend\Tickets\Ticket_controller;
 use App\Models\Product_Category;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
-
+require __DIR__.'/Frontend.php';
 /*Backend Route*/
 Route::get('/admin/login', [AdminController::class, 'login_form'])->name('admin.login');
 Route::post('login-functionality',[AdminController::class,'login_functionality'])->name('login.functionality');
-
 Route::group(['middleware'=>'admin'],function(){
-    Route::get('/',[AdminController::class,'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/dashboard',[AdminController::class,'dashboard'])->name('admin.dashboard');
     Route::get('admin/logout',[AdminController::class,'logout'])->name('admin.logout');
     Route::post('/admin/get_dashboard_data',[AdminController::class,'get_data'])->name('admin.dashboard_get_all_data');
 
@@ -288,7 +287,7 @@ Route::group(['middleware'=>'admin'],function(){
                 Route::post('/delete', 'delete')->name('admin.tickets.assign.delete');
                 Route::post('/store', 'store')->name('admin.tickets.assign.store');
                 Route::post('/update/{id}', 'update')->name('admin.tickets.assign.update');
-                
+
             });
         });
          /*Ticket Route To */
